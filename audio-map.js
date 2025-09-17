31409af25b5e51b0529469d9a52c35b743cb2530
rachelpearlstuart-eng <rachel.pearl.stuart@gmail.com>
2025-09-17 18:51:22 -0400

    Create audio-map.js

---
+++ audio-map.js
@@ -0,0 +1,5 @@
+const audioMap = {
+    'your-filename-here-without-ext': 'https://drive.google.com/uc?export=download&id=YOUR_FILE_ID_HERE',
+    'another-filename': 'https://drive.google.com/uc?export=download&id=ANOTHER_FILE_ID',
+    // ... add all 40 of your files here ...
+};
