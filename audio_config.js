a458d11073b7f66e820d252b3db1e8ee7eb4415d
rachelpearlstuart-eng <rachel.pearl.stuart@gmail.com>
2025-09-18 12:46:34 -0400

    Delete audio_config.js

--- audio_config.js
+++
@@ -1,4 +0,0 @@
-export const externalAudioMap = {
-    'hejinian.raworth.sfart.1999': 'https://archive.org/download/lyn-hejinian-and-tom-raworth-3-3-1999-1001/Lyn_Hejinian_and_Tom_Raworth_3-3-1999_%231001.mp3',
-    'Hejinian_Lyn_naropa_95': 'https://archive.org/download/hejinian.naropa.95/hejinian.naropa.95.mp3',
-};
